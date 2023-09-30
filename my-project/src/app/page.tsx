6f903e6bc78b18ed96c3e06eeb5699cd198c8d26
Nekzus <85507589+Nekzus@users.noreply.github.com>
2023-09-29 23:00:48 -0300

    fix(release-port): relocation of the release-port function

--- my-project/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="rounded-xl p-10 text-center text-5xl neon-violet">Hello World</h1>
-    </main>
-  );
-}
