4c3c9922bbca90d9518202b30d3b430b243b137f
Nekzus <85507589+Nekzus@users.noreply.github.com>
2023-09-29 22:44:33 -0300

    fix(bug-fix): bug fix templateDirectory

---
+++ my-project/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="rounded-xl p-10 text-center text-5xl neon-violet">Hello World</h1>
+    </main>
+  );
+}
