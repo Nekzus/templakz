ff5c15502c16fc86ea721f572e9fdf9a6d5e9841
Nekzus <85507589+Nekzus@users.noreply.github.com>
2023-11-12 14:14:33 -0300

    feat(next14): add next 14 template

--- lib/templates/next-eslint-ts-tw/src/app/page.tsx
+++ lib/templates/next-eslint-ts-tw/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="rounded-xl p-10 text-center text-5xl neon-violet">Hello World</h1>
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <h1 className="p-10 text-5xl text-center rounded-xl neon-violet">Hello World</h1>
     </main>
   );
 }
--- lib/templates/next-eslint-ts-tw/tailwind.config.ts
+++ lib/templates/next-eslint-ts-tw/tailwind.config.ts
@@ -1,12 +1,14 @@
 import type { Config } from "tailwindcss";
+
 import colors from "tailwindcss/colors";
 import plugin from "tailwindcss/plugin";
 
-interface NeonUtilities {
-  [key: string]: {
+type NeonUtilities = Record<
+  string,
+  {
     boxShadow: string;
-  };
-}
+  }
+>;
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,23 +18,24 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: { ...colors.violet, DEFAULT: colors.violet["500"] },
+        primary: {...colors.violet, DEFAULT: colors.violet["500"]},
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [
-    plugin(({ theme, addUtilities }) => {
+    plugin(({theme, addUtilities}) => {
       const neonUtilities: NeonUtilities = {};
       const colors = theme("colors");
+
       for (const color in colors) {
         if (typeof colors[color] === "object") {
           const color1 = colors[color]["500"];
           const color2 = colors[color]["700"];
+
           neonUtilities[`.neon-${color}`] = {
             boxShadow: `0 0 5px ${color1}, 0 0 20px ${color2}`,
           };
@@ -42,4 +45,5 @@ const config: Config = {
     }),
   ],
 };
+
 export default config;
